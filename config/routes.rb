5e7a481eeb4f2877e6a4347557acb01e31192124
Paras Sanghavi <paras@codeforamerica.org>
2017-05-18 17:12:58 -0700

    Facebook oauth with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'visitors#index'
   resources :profiles
+  devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 end
